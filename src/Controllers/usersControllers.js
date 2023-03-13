000b838e20258b216687106d97178cc84b28a407
ygor <ygor.rochaferreira@gmail.com>
2023-03-13 10:43:17 -0300

    Users Update

--- src/Controllers/usersControllers.js
+++ src/Controllers/usersControllers.js
@@ -1,18 +1,62 @@
+const {hash} = require('bcryptjs');
 const AppError = require("../utils/AppError");
 
+const sqliteConnection = require("../database/sqlite")
+
 class usersControllers {
-    create(request, response) {
+   async create(request, response) {
         const {name, email, password} = request.body;
         
-        if (!name) {
-            throw new AppError("Please provide a name")
+        const database = await sqliteConnection()
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+       
+        if (checkUserExists) {
+            throw new AppError("User already exists");
         }
 
-        response.status(201).json({name, email, password});
+        const hashPassword = await hash(password, 8)
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, hashPassword]);
+
+        return response.status(201).json();
+    }
+
+    async update(request, response){
+        
+        const {name, email} = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if (!user) {
+            throw new AppError("User not found")
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+        
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("this email already exists")
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE id = ?
+        `, [user.name, user.email, new Date(), id]);
+
+        return response.json();
     }
 }
 
 module.exports = usersControllers
+
+
 
 
 
